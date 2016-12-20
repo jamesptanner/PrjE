84893a5d0ae3be67374b4b226fedf362688347c9
James Tanner <Jamesptanner@outlook.com>
2016-12-20 22:45:11 +0000

    fixed a character encoding error.

--- EulerQuestions/Euler031/Euler031.h
+++ EulerQuestions/Euler031/Euler031.h
@@ -1,12 +1,12 @@
 //
 //Problem 31: Coin sums
-//In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+//In England the currency is made up of pound, Â£, and pence, p, and there are eight coins in general circulation:
 //
-//1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
-//It is possible to make £2 in the following way:
+//1p, 2p, 5p, 10p, 20p, 50p, Â£1 (100p) and Â£2 (200p).
+//It is possible to make Â£2 in the following way:
 //
-//1£1 + 150p + 220p + 15p + 12p + 31p
-//How many different ways can £2 be made using any number of coins?
+//1Â£1 + 150p + 220p + 15p + 12p + 31p
+//How many different ways can Â£2 be made using any number of coins?
 
 #include "EulerShared.h"
 
